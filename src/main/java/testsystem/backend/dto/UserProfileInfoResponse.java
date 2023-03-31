7bb25b21c9e30509b3a6486ed560ddac2c1bdc7c
ShvDanil <dishvetsov@edu.hse.ru>
2023-03-31 17:42:14 +0300

    Creating profile of user dto.

---
+++ src/main/java/testsystem/backend/dto/UserProfileInfoResponse.java
@@ -0,0 +1,32 @@
+package testsystem.backend.dto;
+
+import jakarta.annotation.Nullable;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileInfoResponse {
+
+    @NonNull
+    private Integer id;
+
+    @NonNull
+    private String first_name;
+
+    @NonNull
+    private String last_name;
+
+    @Nullable
+    private String middle_name;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private String department;
+
+    @NonNull
+    private String group;
+}

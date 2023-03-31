f61d824909d52d50df7377e66a87b4218ab25f69
ShvDanil <dishvetsov@edu.hse.ru>
2023-03-31 17:35:19 +0300

    Creating main program entry point and main test entry point.

---
+++ src/test/java/testsystem/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package testsystem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

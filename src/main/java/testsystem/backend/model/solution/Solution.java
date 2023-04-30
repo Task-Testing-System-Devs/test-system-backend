f3081dde80b6c99c7e7b6665957aba42c70f858f
ShvDanil <dishvetsov@edu.hse.ru>
2023-04-30 22:51:24 +0300

    Creating solution model respective to solutions db table.

---
+++ src/main/java/testsystem/backend/model/solution/Solution.java
@@ -0,0 +1,55 @@
+package testsystem.backend.model.solution;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "solutions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Solution {
+
+    @Column(name = "solution_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Integer id;
+
+    @Column(name = "unique_task_id")
+    @Getter
+    @Setter
+    private Integer uniqueTaskId;
+
+    @Column(name = "code")
+    @Getter
+    @Setter
+    private String code;
+
+    @Column(name = "language_id")
+    @Getter
+    @Setter
+    private Integer languageId;
+
+    @Column(name = "status_id")
+    @Getter
+    @Setter
+    private Integer statusId;
+
+    @Column(name = "used_time")
+    @Getter
+    @Setter
+    private Double usedTime;
+
+    @Column(name = "used_memory")
+    @Getter
+    @Setter
+    private Double usedMemory;
+
+    @Column(name = "error_test")
+    @Getter
+    @Setter
+    private String errorTest;
+
+}

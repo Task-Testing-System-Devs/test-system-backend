c793bf10cf84f9e92bbacb8194fdd43f28086156
ShvDanil <dishvetsov@edu.hse.ru>
2023-04-30 22:52:54 +0300

    Adding search by title method to task repository.

--- src/main/java/testsystem/backend/repository/contest/TaskRepository.java
+++ src/main/java/testsystem/backend/repository/contest/TaskRepository.java
@@ -3,5 +3,10 @@ package testsystem.backend.repository.contest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import testsystem.backend.model.contest.Task;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    Optional<Task> findByTitle(String title);
+
 }

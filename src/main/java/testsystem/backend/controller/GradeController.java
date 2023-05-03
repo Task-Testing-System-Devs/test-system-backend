a5b08dd1f82dd913465cee5e3bb4d264c87bcce8
ShvDanil <dishvetsov@edu.hse.ru>
2023-05-03 15:52:07 +0300

    Adding comments to grade controller.

--- src/main/java/testsystem/backend/controller/GradeController.java
+++ src/main/java/testsystem/backend/controller/GradeController.java
@@ -7,26 +7,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import testsystem.backend.model.user.UserInfoForRatingDownload;
 import testsystem.backend.service.GradeService;
-import testsystem.backend.service.JwtService;
 import testsystem.backend.utilities.FileService;
 
 import java.util.List;
 
+/**
+ * Controller class handling requests related to grades.
+ */
 @RestController
 @RequestMapping("/api/grade")
 public class GradeController {
 
-    @Autowired
-    private JwtService jwtService;
+    // Constants for file names.
+    private static final String RATING_MARKS_NAME = "rating_marks.csv";
+    private static final String RATING_TASKS_AMOUNT_NAME = "rating_tasks_amount.csv";
+
+    // FileService instance for file handling.
+    private final FileService fileService = new FileService();
 
     @Autowired
     private GradeService gradeService;
 
+    /**
+     * Returns ResponseEntity with grades sorted by marks.
+     *
+     * @return ResponseEntity with grades sorted by marks, or a bad request if an exception occurs.
+     */
     @GetMapping("/by-marks")
     public ResponseEntity<?> getGradesByMarks() {
         try {
@@ -36,23 +46,11 @@ public class GradeController {
         }
     }
 
-    @GetMapping("/by-marks/download-csv")
-    public ResponseEntity<?> downloadCSVRatingByMarks() {
-        try {
-            List<UserInfoForRatingDownload> userInfoForRatingDownloadList = gradeService.getSortedByMarksUsersForDownload();
-            FileService fileService = new FileService();
-            fileService.convertToCSV(userInfoForRatingDownloadList.get(0).toString(), userInfoForRatingDownloadList, "rating_marks.csv");
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.setContentDispositionFormData("attachment", "rating_marks.csv");
-            FileSystemResource file = new FileSystemResource("rating_marks.csv");
-            return new ResponseEntity<>(file, headers, HttpStatus.OK);
-        } catch (Exception exception) {
-            return ResponseEntity.badRequest().body(exception.getMessage());
-        }
-    }
-
+    /**
+     * Returns ResponseEntity with grades sorted by tasks amount.
+     *
+     * @return ResponseEntity with grades sorted by tasks amount, or a bad request if an exception occurs.
+     */
     @GetMapping("/by-tasks-amount")
     public ResponseEntity<?> getGradesByTasksAmount() {
         try {
@@ -62,23 +60,61 @@ public class GradeController {
         }
     }
 
-    @GetMapping("/by-tasks-amount/download-csv")
-    public ResponseEntity<?> downloadCSVRatingByTasksAmount() {
+    /**
+     * Downloads a CSV file with grades sorted by marks.
+     *
+     * @return ResponseEntity with the CSV file as a FileSystemResource, or a bad request if an exception occurs.
+     */
+    @GetMapping("/by-marks/download-csv")
+    public ResponseEntity<?> downloadCSVRatingByMarks() {
         try {
-            List<UserInfoForRatingDownload> userInfoForRatingDownloadList = gradeService.getSortedByTasksAmountUsersForDownload();
-            FileService fileService = new FileService();
-            fileService.convertToCSV(userInfoForRatingDownloadList.get(0).toString(), userInfoForRatingDownloadList, "rating_tasks_amount.csv");
+            // Get the list of users with grades sorted by marks and convert it to CSV.
+            List<UserInfoForRatingDownload> userInfoForRatingDownloadList = gradeService.getSortedByMarksUsersForDownload();
 
+            fileService.convertToCSV(
+                    userInfoForRatingDownloadList.get(0).toString(),
+                    userInfoForRatingDownloadList, RATING_MARKS_NAME
+            );
+
+            // Set the headers for the ResponseEntity with the CSV file.
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.setContentDispositionFormData("attachment", "rating_tasks_amount.csv");
-            FileSystemResource file = new FileSystemResource("rating_tasks_amount.csv");
+            headers.setContentDispositionFormData("attachment", RATING_MARKS_NAME);
 
+            // Create the FileSystemResource with the CSV file and return the ResponseEntity.
+            FileSystemResource file = new FileSystemResource(RATING_MARKS_NAME);
             return new ResponseEntity<>(file, headers, HttpStatus.OK);
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
     }
 
+    /**
+     * Downloads a CSV file with grades sorted by tasks amount.
+     *
+     * @return ResponseEntity with the CSV file as a FileSystemResource, or a bad request if an exception occurs.
+     */
+    @GetMapping("/by-tasks-amount/download-csv")
+    public ResponseEntity<?> downloadCSVRatingByTasksAmount() {
+        try {
+            // Get the list of users with grades sorted by tasks amount and convert it to CSV.
+            List<UserInfoForRatingDownload> userInfoForRatingDownloadList = gradeService.getSortedByTasksAmountUsersForDownload();
 
+            fileService.convertToCSV(
+                    userInfoForRatingDownloadList.get(0).toString(),
+                    userInfoForRatingDownloadList, RATING_TASKS_AMOUNT_NAME
+            );
+
+            // Set the headers for the ResponseEntity with the CSV file.
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", RATING_TASKS_AMOUNT_NAME);
+
+            // Create the FileSystemResource with the CSV file and return the ResponseEntity.
+            FileSystemResource file = new FileSystemResource(RATING_TASKS_AMOUNT_NAME);
+            return new ResponseEntity<>(file, headers, HttpStatus.OK);
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
 }

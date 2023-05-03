ab26346598afdbe783068471e3b0d826c9c365c6
ShvDanil <dishvetsov@edu.hse.ru>
2023-05-03 15:56:33 +0300

    Adding comments to profile info controller.

--- src/main/java/testsystem/backend/controller/ProfileController.java
+++ src/main/java/testsystem/backend/controller/ProfileController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.*;
 import testsystem.backend.service.JwtService;
 import testsystem.backend.service.ProfileService;
 
+/**
+ * Controller class for profile-related operations.
+ */
 @RestController
 @RequestMapping("/api/profile")
 public class ProfileController {
@@ -15,26 +18,41 @@ public class ProfileController {
     @Autowired
     private ProfileService profileService;
 
+    /**
+     * Returns the student's profile information based on the token header.
+     *
+     * @param token Authorization header containing the token.
+     * @return ResponseEntity containing the student's profile information in the body or an error message if any exceptions occur.
+     */
     @GetMapping("/get-student-info")
     public ResponseEntity<?> getStudentProfileInfo(
             @RequestHeader("Authorization") String token
     ) {
         try {
-            return profileService.getStudentProfileInfo(jwtService.extractUsername(token.replace("Bearer ", "")));
+            return profileService.getStudentProfileInfo(
+                    jwtService.extractUsername(token.replace("Bearer ", ""))
+            );
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
     }
 
+    /**
+     * Returns the teacher's profile information based on the token header.
+     *
+     * @param token Authorization header containing the token.
+     * @return ResponseEntity containing the teacher's profile information in the body or an error message if any exceptions occur.
+     */
     @GetMapping("/get-teacher-info")
     public ResponseEntity<?> getTeacherProfileInfo(
             @RequestHeader("Authorization") String token
     ) {
         try {
-            return profileService.getTeacherProfileInfo(jwtService.extractUsername(token.replace("Bearer ", "")));
+            return profileService.getTeacherProfileInfo(
+                    jwtService.extractUsername(token.replace("Bearer ", ""))
+            );
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
     }
-
 }

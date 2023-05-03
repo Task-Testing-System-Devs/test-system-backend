a6272bac7a5c2349edc0aef4a447932ac2bc0628
ShvDanil <dishvetsov@edu.hse.ru>
2023-05-03 16:04:36 +0300

    Adding comments to solution controller.

--- src/main/java/testsystem/backend/controller/SolutionController.java
+++ src/main/java/testsystem/backend/controller/SolutionController.java
@@ -7,6 +7,9 @@ import testsystem.backend.dto.SolutionDTOObject;
 import testsystem.backend.service.JwtService;
 import testsystem.backend.service.SolutionService;
 
+/**
+ * Controller class for solutions-related operations.
+ */
 @RestController
 @RequestMapping("/api/solutions")
 public class SolutionController {
@@ -16,27 +19,43 @@ public class SolutionController {
     @Autowired
     private SolutionService solutionService;
 
+    /**
+     * Retrieves all user solutions.
+     *
+     * @param token Authorization header containing the token.
+     * @return ResponseEntity with all retrieved user solutions in the response body.
+     */
     @GetMapping("/get-all")
     public ResponseEntity<?> getUserSolutions(
             @RequestHeader("Authorization") String token
     ) {
         try {
-            return solutionService.getUserSolutions(jwtService.extractUsername(token.replace("Bearer ", "")));
+            return solutionService.getUserSolutions(
+                    jwtService.extractUsername(token.replace("Bearer ", ""))
+            );
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
     }
 
+    /**
+     * Adds new user solution.
+     *
+     * @param solutionDTOObject DTO object containing the user's solution details.
+     * @return ResponseEntity with the adding user solution operation status.
+     */
     @PostMapping("/add")
     public ResponseEntity<?> addUserSolution(
             @RequestHeader("Authorization") String token,
             @RequestBody SolutionDTOObject solutionDTOObject
             ) {
         try {
-            return solutionService.addUserSolution(jwtService.extractUsername(token.replace("Bearer ", "")), solutionDTOObject);
+            return solutionService.addUserSolution(
+                    jwtService.extractUsername(token.replace("Bearer ", "")),
+                    solutionDTOObject
+            );
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
     }
-
 }

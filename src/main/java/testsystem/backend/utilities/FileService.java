4251e01f535d3a95c476059faef9d91c0229f7b6
ShvDanil <dishvetsov@edu.hse.ru>
2023-05-03 18:33:45 +0300

    Adding comments to file service.

--- src/main/java/testsystem/backend/utilities/FileService.java
+++ src/main/java/testsystem/backend/utilities/FileService.java
@@ -6,12 +6,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * A utility class for converting a list of {@link testsystem.backend.model.user.UserInfoForRatingDownload} objects
+ * to a CSV file.
+ */
 public class FileService {
 
+    /**
+     * Converts a list of {@link testsystem.backend.model.user.UserInfoForRatingDownload} objects to a CSV file.
+     * @param headers Headers of the CSV file.
+     * @param models List of {@link testsystem.backend.model.user.UserInfoForRatingDownload} objects to be converted.
+     * @param filename Name of the CSV file to be created.
+     * @throws IOException If an I/O error occurs while writing to the CSV file.
+     */
     public void convertToCSV(String headers, List<UserInfoForRatingDownload> models, String filename) throws IOException {
         try (FileWriter csvWriter = new FileWriter(filename)) {
             csvWriter.write(headers + "\n");
 
+            // Convert each model to a CSV row and write it to the file.
             for (var model : models) {
                 csvWriter.append(model.getRatingPosition().toString())
                         .append(";")
@@ -28,5 +40,4 @@ public class FileService {
             }
         }
     }
-
 }

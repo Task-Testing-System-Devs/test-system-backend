aa8656b89efbd9d1e4bfc428e1945d54597def13
ShvDanil <dishvetsov@edu.hse.ru>
2023-05-06 16:05:13 +0300

    Adding one more function to repo.

--- src/main/java/testsystem/backend/repository/contest/ContestConnRepository.java
+++ src/main/java/testsystem/backend/repository/contest/ContestConnRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import testsystem.backend.model.contest.ContestConn;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Repository corresponding to ContestConn model and same table in database.
  */
 public interface ContestConnRepository extends JpaRepository<ContestConn, Integer> {
     List<ContestConn> findAllByUserId(Integer id);
+
+    boolean existsByUserId(Integer id);
 }
